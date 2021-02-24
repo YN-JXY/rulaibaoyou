c4578144b99d7486c5ce03c69e8d32fe9bfd1c55
Name <2194457627@qq.com>
2021-02-24 18:11:38 +0800

    "2月24日下班啦"

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,12 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>主页</h1>
-</body>
-</html>
+@extends('layouts.default')
+
+@section('title')
+    home
+@endsection
+
+@section('content')
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            你现在所看到的是<a href="https://learnku.com/courses/laravel-essential-training">
+            Laravel入门教程</a>的示例项目主页。    
+        </p>
+        <p>
+            一切，将从这里开始。
+        </p>
+        <p>
+            {{-- <a href="btn btn-lg btn-success" href="#" role="button">现在注册</a> --}}
+            <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+        </p>
+    </div>
+@stop    
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\StaticPagesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/','StaticPagesController@home');
-Route::get('/help','StaticPagesController@help');
-Route::get('/about','StaticPagesController@about');
+// Route::get('/','StaticPagesController@home');
+Route::get('/',[StaticPagesController::class,'home']);
+Route::get('/help',[StaticPagesController::class,'help']);
+Route::get('/about',[StaticPagesController::class,'about']);
